ed3b5ac515631d04e1f592880d6af90f51ca4323
Lee Siyun <118098885+liv303@users.noreply.github.com>
2023-06-30 16:25:29 +0900

    Add files via upload

---
+++ src/main/java/com/example/Miniproject_Market/MiniprojectMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.Miniproject_Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojectMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojectMarketApplication.class, args);
+	}
+
+}
