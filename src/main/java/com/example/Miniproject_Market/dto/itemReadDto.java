3a3ef6b9a3a02e772ec13ce5c3a2e4b7b2e6f502
Leesiyun <livre303@gmail.com>
2023-07-02 04:23:46 +0900

    feat: 페이지 조회용 Dto 분리

---
+++ src/main/java/com/example/Miniproject_Market/dto/itemReadDto.java
@@ -0,0 +1,23 @@
+package com.example.Miniproject_Market.dto;
+
+import com.example.Miniproject_Market.Entity.itemEntity;
+import lombok.Data;
+@Data
+public class itemReadDto {
+    private Long id;
+    private String title;
+    private String description;
+    private Integer minPriceWanted;
+    private String status;
+
+    public static itemReadDto readEntity(itemEntity entity) {
+        itemReadDto readDto = new itemReadDto();
+        readDto.setId(entity.getId());
+        readDto.setTitle(entity.getTitle());
+        readDto.setDescription(entity.getDescription());
+        readDto.setMinPriceWanted(entity.getMinPriceWanted());
+        readDto.setStatus(entity.getStatus());
+        return readDto;
+    }
+}
+
